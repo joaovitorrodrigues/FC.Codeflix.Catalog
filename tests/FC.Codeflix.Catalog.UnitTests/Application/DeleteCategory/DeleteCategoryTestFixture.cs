c9331c37625a8abbadb3d2a0129c5d47c80b0042
João Vitor Rodrigues <jaozitus@gmail.com>
2025-02-04 20:19:06 -0300

    Added DeleteCategoryTest

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.UnitTests.Common;
+using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.DeleteCategory
+{
+    [CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+    public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture> { }
+    public class DeleteCategoryTestFixture : BaseFixture
+    {
+        public Mock<ICategoryRepository> GetRepositoryMock() => new();
+        public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+
+        public string GetValidCategoryName()
+        {
+            string categoryName = "";
+            while (categoryName.Length < 3)
+            {
+                categoryName = Faker.Commerce.Categories(1)[0];
+            }
+            if (categoryName.Length > 255)
+                categoryName = categoryName[..255];
+
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            string categoryDescription = "";
+            categoryDescription = Faker.Commerce.ProductDescription();
+            if (categoryDescription.Length > 10000)
+                categoryDescription = categoryDescription[..10000];
+
+            return categoryDescription;
+        }
+
+        public Category GetValidCategory() => new(GetValidCategoryName(), GetValidCategoryDescription());
+    }
+}
