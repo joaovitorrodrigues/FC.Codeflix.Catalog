ad7070abadff0e09710072a6ac62b6639ee616e5
João Vitor Rodrigues <jaozitus@gmail.com>
2025-02-04 19:32:04 -0300

    added  GetCategory Exception Test

---
+++ src/FC.Codeflix.Catalog.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace FC.Codeflix.Catalog.Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string? message) : base(message)
+        {
+        }
+    }
+}
--- tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
@@ -1,4 +1,5 @@
-﻿using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
 using FluentAssertions;
 using Moq;
 using UseCase = FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
@@ -37,6 +38,27 @@ namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory
             output.IsActive.Should().Be(exampleCategory.IsActive);
             output.Id.Should().Be(exampleCategory.Id);
             (output.CreatedAt != default).Should().BeTrue();
+        }
+
+        [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExist))]
+        [Trait("Application", "GetCategory - Use Cases")]
+        public async Task NotFoundExceptionWhenCategoryDoesntExist()
+        {
+            var repositoryMock = _fixture.GetRepositoryMock();
+            var exampleGuid = Guid.NewGuid();
+
+            repositoryMock.Setup(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()
+                )).ThrowsAsync(new NotFoundException($"Category '{exampleGuid}' not found"));
+
+            var input = new GetCategoryInput(exampleGuid);
+            var useCase = new UseCase.GetCategory(repositoryMock.Object);
+
+            var task = async () => await useCase.Handle(input, CancellationToken.None);
+            await task.Should().ThrowAsync<NotFoundException>();
+            repositoryMock.Verify(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
+
+            
+
         }
     }
 }
