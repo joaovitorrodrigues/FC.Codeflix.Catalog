8f2b3e9a6f51441155c39f78ad87853de2dc649a
João Vitor Rodrigues <jaozitus@gmail.com>
2025-02-05 12:47:09 -0300

    Refactoring UpdatedCategory added DataGenerator

--- tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
@@ -1,4 +1,6 @@
 ﻿using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FC.Codeflix.Catalog.Domain.Entity;
 using FluentAssertions;
 using Moq;
 using UseCases = FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
@@ -14,33 +16,30 @@ namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory
             _fixture = fixture;
         }
 
-        [Fact(DisplayName =nameof(UpdateCategory))]
+        [Theory(DisplayName =nameof(UpdateCategory))]
         [Trait("Application", "UpdateCategory - Use Cases")]
-        public async Task UpdateCategory()
+        [MemberData(
+            nameof(UpdateCategoryTestDataGenerator.GetCategoriesToUpdate), 
+            parameters: 10, 
+            MemberType = typeof(UpdateCategoryTestDataGenerator)
+        )]
+        public async Task UpdateCategory(Category exampleCategory, UpdateCategoryInput exampleInput)
         {
             var repositoryMock = _fixture.GetRepositoryMock();
             var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
 
-            var exampleCategory = _fixture.GetValidCategory();
 
             repositoryMock.Setup(x => x.Get(exampleCategory.Id, It.IsAny<CancellationToken>())).ReturnsAsync(exampleCategory);
 
-            var input = new UseCases.UpdateCategoryInput
-            (
-                exampleCategory.Id,
-                _fixture.GetValidCategoryName(),
-                _fixture.GetValidCategoryDescription(),
-                !exampleCategory.IsActive
-            );
 
             var useCase = new UseCases.UpdateCategory(repositoryMock.Object, unitOfWorkMock.Object);
 
-            CategoryModelOutput output = await useCase.Handle(input, CancellationToken.None);
+            CategoryModelOutput output = await useCase.Handle(exampleInput, CancellationToken.None);
 
             output.Should().NotBeNull();
-            output.Name.Should().Be(input.Name);
-            output.Description.Should().Be(input.Description);
-            output.IsActive.Should().Be(input.IsActive);
+            output.Name.Should().Be(exampleInput.Name);
+            output.Description.Should().Be(exampleInput.Description);
+            output.IsActive.Should().Be(exampleInput.IsActive);
 
             repositoryMock.Verify(x => x.Get(exampleCategory.Id, It.IsAny<CancellationToken>()), Times.Once);
             repositoryMock.Verify(x => x.Update(exampleCategory, It.IsAny<CancellationToken>()), Times.Once);
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestDataGenerator.cs
@@ -0,0 +1,32 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory
+{
+    public class UpdateCategoryTestDataGenerator
+    {
+        public static IEnumerable<object[]> GetCategoriesToUpdate(int times = 10)
+        {
+            var fixture = new UpdateCategoryTestFixture();
+
+            for (int i = 0; i < times; i++)
+            {
+                var exampleCategory = fixture.GetValidCategory();
+
+                var exampleInput = new UpdateCategoryInput(
+                    exampleCategory.Id,
+                    fixture.GetValidCategoryName(),
+                    fixture.GetValidCategoryDescription(),
+                    fixture.GetRandomBoolean()
+                );
+
+                yield return new object[]
+                {
+                    exampleCategory,
+                    exampleInput
+                };
+
+            }
+
+        }
+    }
+}
