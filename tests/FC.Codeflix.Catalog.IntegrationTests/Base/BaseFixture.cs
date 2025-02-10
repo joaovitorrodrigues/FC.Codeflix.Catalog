009d8509983b43cc93611c93a9ef7c2e38588b6a
João Vitor Rodrigues <jaozitus@gmail.com>
2025-02-10 14:40:09 -0300

    Added base integration tests

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Base/BaseFixture.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Base
+{
+    public abstract class BaseFixture
+    {
+        protected Faker Faker { get; set; }
+        public BaseFixture()
+        {
+            Faker = new Faker("pt_BR");
+        }
+    }
+}
