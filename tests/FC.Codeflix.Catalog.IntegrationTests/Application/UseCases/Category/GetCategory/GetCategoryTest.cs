1872ed0a527f78a06709542bd89b6fdf9d7f7dd0
João Vitor Rodrigues <jaozitus@gmail.com>
2025-02-14 12:27:09 -0300

    Added integration not found get category test

--- tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTest.cs
@@ -1,4 +1,6 @@
-﻿using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
 using FluentAssertions;
 using UseCase = FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
 namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.GetCategory
@@ -15,9 +17,9 @@ namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Get
         public async Task GetCategory()
         {
             var dbContext = _fixture.CreateDbContext();
-            
+
             var exampleCategory = _fixture.GetExampleCategory();
-            
+
             dbContext.Categories.Add(exampleCategory);
             dbContext.SaveChanges();
 
@@ -28,12 +30,33 @@ namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Get
 
             var output = await useCase.Handle(input, CancellationToken.None);
 
-            
+
             output.Should().NotBeNull();
             output.Name.Should().Be(exampleCategory.Name);
             output.Description.Should().Be(exampleCategory.Description);
             output.Id.Should().Be(exampleCategory.Id);
             (output.CreatedAt != default).Should().BeTrue();
+        }
+
+        [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExist))]
+        [Trait("Integration/Application", "GetCategory - Use Cases")]
+        public async Task NotFoundExceptionWhenCategoryDoesntExist()
+        {
+            var dbContext = _fixture.CreateDbContext();
+
+            var exampleCategory = _fixture.GetExampleCategory();
+
+            dbContext.Categories.Add(exampleCategory);
+            dbContext.SaveChanges();
+
+            var repository = new CategoryRepository(dbContext);
+
+            var input = new UseCase.GetCategoryInput(Guid.NewGuid());
+            var useCase = new UseCase.GetCategory(repository);
+
+            var task = async () => await useCase.Handle(input, CancellationToken.None);
+            await task.Should().ThrowAsync<NotFoundException>();
+
         }
     }
 }
