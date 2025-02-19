c61836bb3a7c242a4527301b60027b854362bfbc
João Vitor Rodrigues <jaozitus@gmail.com>
2025-02-19 15:08:34 -0300

    Added integration DeleteCategory and ListCategories tests

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTest.cs
@@ -0,0 +1,68 @@
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using ApplicationUseCases = FC.Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.DeleteCategory
+{
+    [Collection(nameof(DeleteCategoryTestFixture))]
+    public class DeleteCategoryTest
+    {
+        private readonly DeleteCategoryTestFixture _fixture;
+
+        public DeleteCategoryTest(DeleteCategoryTestFixture fixture)
+        => _fixture = fixture;
+
+
+        [Fact(DisplayName = nameof(DeleteCategory))]
+        [Trait("Integration/Application", "DeleteCategory - Use Cases")]
+        public async Task DeleteCategory()
+        {
+            var dbContext = _fixture.CreateDbContext();
+            var repository = new CategoryRepository(dbContext);
+            var unitOfWork = new UnitOfWork(dbContext);
+            var exampleList = _fixture.GetExampleCategoriesList(10);
+
+            await dbContext.AddRangeAsync(exampleList);
+            var categoryExample = _fixture.GetExampleCategory();
+            var tracking = await dbContext.AddAsync(categoryExample);
+            dbContext.SaveChanges();
+            tracking.State = EntityState.Detached;
+
+            var input = new ApplicationUseCases.DeleteCategoryInput(categoryExample.Id);
+
+            var useCase = new ApplicationUseCases.DeleteCategory(repository, unitOfWork);
+
+            await useCase.Handle(input, CancellationToken.None);
+
+            var assertDbContext = _fixture.CreateDbContext(true);
+            var dbCategoryDeleted = await assertDbContext.Categories.FindAsync(categoryExample.Id);
+            var dbCategories = await assertDbContext.Categories.ToListAsync();
+            
+            dbCategoryDeleted.Should().BeNull();          
+            dbCategories.Should().HaveCount(exampleList.Count);
+        }
+
+        [Fact(DisplayName = nameof(ThrowWhenCategoryNotFound))]
+        [Trait("Integration/Application", "DeleteCategory - Use Cases")]
+        public async Task ThrowWhenCategoryNotFound()
+        {
+            var dbContext = _fixture.CreateDbContext();
+            var repository = new CategoryRepository(dbContext);
+            var unitOfWork = new UnitOfWork(dbContext);
+
+            var exampleGuid = Guid.NewGuid();
+
+            var input = new ApplicationUseCases.DeleteCategoryInput(exampleGuid);
+
+            var useCase = new ApplicationUseCases.DeleteCategory(repository, unitOfWork);
+
+            var task = async () => await useCase.Handle(input, CancellationToken.None);
+
+            await task.Should().ThrowAsync<NotFoundException>().WithMessage($"Category '{input.Id}' not found.");
+
+        }
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+using EntityDomain = FC.Codeflix.Catalog.Domain.Entity;
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.ListCategories
+{
+    [CollectionDefinition(nameof(ListCategoriesTestFixture))]
+    public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCategoriesTestFixture> { }
+    public class ListCategoriesTestFixture : CategoryUseCasesBaseFixture
+    {
+        public List<EntityDomain.Category> GetExampleCategoriesListWithNames(List<string> names)
+            => names.Select(name =>
+            {
+                var category = GetExampleCategory();
+                category.Update(name);
+                return category;
+            }).ToList();
+
+        public List<EntityDomain.Category> CloneCategoriesListOrdered(List<EntityDomain.Category> categoriesList, string orderBy, SearchOrder order)
+        {
+            var listClone = new List<EntityDomain.Category>(categoriesList);
+            IOrderedEnumerable<EntityDomain.Category> orderedEnumerable = (orderBy, order) switch
+            {
+                ("name", SearchOrder.Asc) => listClone.OrderBy(x => x.Name),
+                ("name", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Name),
+                ("id", SearchOrder.Asc) => listClone.OrderBy(x => x.Id),
+                ("id", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Id),
+                ("createdat", SearchOrder.Asc) => listClone.OrderBy(x => x.CreatedAt),
+                ("createdat", SearchOrder.Desc) => listClone.OrderByDescending(x => x.CreatedAt),
+                _ => listClone.OrderBy(x => x.Name),
+            };
+
+            return orderedEnumerable.ToList();
+        }
+    }
+}
