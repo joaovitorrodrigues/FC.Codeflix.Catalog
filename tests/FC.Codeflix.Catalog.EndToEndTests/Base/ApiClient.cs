895f155b7a4dad91d9619f9beda6d91166422d3b
João Vitor Rodrigues <jaozitus@gmail.com>
2025-03-13 19:58:14 -0300

    update api endpoints tests -> camel case to snake case

--- tests/FC.Codeflix.Catalog.EndToEndTests/Base/ApiClient.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Base/ApiClient.cs
@@ -1,84 +1,97 @@
-﻿using Microsoft.AspNetCore.WebUtilities;
+﻿using FC.Codeflix.Catalog.EndToEndTests.Extensions.String;
+using Microsoft.AspNetCore.WebUtilities;
 using System.Text;
 using System.Text.Json;
 
 namespace FC.Codeflix.Catalog.EndToEndTests.Base
 {
-    public class ApiClient
+    class SnakeCaseNamingPolicy : JsonNamingPolicy
     {
-        private readonly HttpClient _httpClient;
+        public override string ConvertName(string name)
+        => name.ToSnakeCase();
 
-        public ApiClient(HttpClient httpClient)
-            => _httpClient = httpClient;
-
-
-        public async Task<(HttpResponseMessage?, TOutput?)> Post<TOutput>(string route, object payload) where TOutput : class
+        public class ApiClient
         {
-            var response = await _httpClient.PostAsync(
-                route,
-                new StringContent(
-                    JsonSerializer.Serialize(payload),
-                    Encoding.UTF8,
-                    "application/json"
-                )
-            );
-            var output = await GetOutPut<TOutput>(response);
+            private readonly HttpClient _httpClient;
+            private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = new SnakeCaseNamingPolicy(),
+                PropertyNameCaseInsensitive = true
+            };
 
-            return (response, output);
-        }
+            public ApiClient(HttpClient httpClient)
+                => _httpClient = httpClient;
 
-        public async Task<(HttpResponseMessage?, TOutput?)> Get<TOutput>(string route, object? queryStringParametersObject = null) where TOutput : class
-        {
-            var url = PrepareGetRoute(route, queryStringParametersObject);
-            var response = await _httpClient.GetAsync(url);
-            var output = await GetOutPut<TOutput>(response);
 
-            return (response, output);
-        }
-        public async Task<(HttpResponseMessage?, TOutput?)> Delete<TOutput>(string route) where TOutput : class
-        {
-            var response = await _httpClient.DeleteAsync(route);
-            var output = await GetOutPut<TOutput>(response);
+            public async Task<(HttpResponseMessage?, TOutput?)> Post<TOutput>(string route, object payload) where TOutput : class
+            {
+                var response = await _httpClient.PostAsync(
+                    route,
+                    new StringContent(
+                        JsonSerializer.Serialize(payload, _jsonSerializerOptions),
+                        Encoding.UTF8,
+                        "application/json"
+                    )
+                );
+                var output = await GetOutPut<TOutput>(response);
 
-            return (response, output);
-        }
-        public async Task<(HttpResponseMessage?, TOutput?)> Put<TOutput>(string route, object payload) where TOutput : class
-        {
-            var response = await _httpClient.PutAsync(
-                route,
-                new StringContent(
-                    JsonSerializer.Serialize(payload),
-                    Encoding.UTF8,
-                    "application/json"
-                )
-            );
-           var output = await GetOutPut<TOutput>(response);
+                return (response, output);
+            }
 
-            return (response, output);
-        }
+            public async Task<(HttpResponseMessage?, TOutput?)> Get<TOutput>(string route, object? queryStringParametersObject = null) where TOutput : class
+            {
+                var url = PrepareGetRoute(route, queryStringParametersObject);
+                var response = await _httpClient.GetAsync(url);
+                var output = await GetOutPut<TOutput>(response);
 
-        private async Task<TOutput?> GetOutPut<TOutput>(HttpResponseMessage response) where TOutput  : class
-        {
-            var outputString = await response.Content.ReadAsStringAsync();
+                return (response, output);
+            }
+            public async Task<(HttpResponseMessage?, TOutput?)> Delete<TOutput>(string route) where TOutput : class
+            {
+                var response = await _httpClient.DeleteAsync(route);
+                var output = await GetOutPut<TOutput>(response);
 
-            TOutput? output = null;
-            if (!string.IsNullOrWhiteSpace(outputString))
-                output = JsonSerializer.Deserialize<TOutput>(outputString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return (response, output);
+            }
+            public async Task<(HttpResponseMessage?, TOutput?)> Put<TOutput>(string route, object payload) where TOutput : class
+            {
+                var response = await _httpClient.PutAsync(
+                    route,
+                    new StringContent(
+                        JsonSerializer.Serialize(payload, _jsonSerializerOptions),
+                        Encoding.UTF8,
+                        "application/json"
+                    )
+                );
+                var output = await GetOutPut<TOutput>(response);
 
-            return output;
-        }
+                return (response, output);
+            }
 
-        private string PrepareGetRoute(string route, object? queryStringParametersObject)
-        {
-            //if (queryStringParametersObject == null)
-            //    return route;
-            //var queryString = string.Join("&", queryStringParametersObject.GetType().GetProperties().Select(x => $"{x.Name}={x.GetValue(queryStringParametersObject)}"));
-            //return $"{route}?{queryString}";
-            if (queryStringParametersObject == null)
-                return route;
-            var parametersJson = JsonSerializer.Serialize(queryStringParametersObject);
-            var parametersDictionary = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(parametersJson);
-            return QueryHelpers.AddQueryString(route, parametersDictionary!);
+            private async Task<TOutput?> GetOutPut<TOutput>(HttpResponseMessage response) where TOutput : class
+            {
+                var outputString = await response.Content.ReadAsStringAsync();
+
+                TOutput? output = null;
+                if (!string.IsNullOrWhiteSpace(outputString))
+                    output = JsonSerializer.Deserialize<TOutput>(outputString, _jsonSerializerOptions);
+
+                return output;
+            }
+
+            private string PrepareGetRoute(string route, object? queryStringParametersObject)
+            {
+                //if (queryStringParametersObject == null)
+                //    return route;
+                //var queryString = string.Join("&", queryStringParametersObject.GetType().GetProperties().Select(x => $"{x.Name}={x.GetValue(queryStringParametersObject)}"));
+                //return $"{route}?{queryString}";
+                if (queryStringParametersObject == null)
+                    return route;
+                var parametersJson = JsonSerializer.Serialize(queryStringParametersObject, _jsonSerializerOptions);
+                var parametersDictionary = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(parametersJson);
+                return QueryHelpers.AddQueryString(route, parametersDictionary!);
+            }
+
+
         }
     }
-}
