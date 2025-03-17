4758380fdd9988fc171963ac1d9fdab4fcc34247
João Vitor Rodrigues <jaozitus@gmail.com>
2025-03-17 20:18:40 -0300

    add first genre entity tests

---
+++ src/FC.Codeflix.Catalog.Domain/Entity/Genre.cs
@@ -0,0 +1,17 @@
+﻿using FC.Codeflix.Catalog.Domain.SeedWork;
+namespace FC.Codeflix.Catalog.Domain.Entity
+{
+    public class Genre : AggregateRoot
+    {
+
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public Genre(string name, bool isActive = true)
+        {
+            Name = name;
+            IsActive = isActive;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using EntityDomain = FC.Codeflix.Catalog.Domain.Entity;
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Genre
+{
+    [Collection(nameof(GenreTestFixture))]
+    public class GenreTest
+    {
+        private readonly GenreTestFixture _fixture;
+
+        public GenreTest(GenreTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Genre - Aggregates")]
+        public void Instantiate()
+        {
+            var genreName = "Horror";
+            var genre = new EntityDomain.Genre(genreName);
+
+            genre.Should().NotBeNull();
+            genre.Name.Should().Be(genreName);
+            genre.IsActive.Should().BeTrue();
+            genre.CreatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+        }
+
+
+        [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+        [Trait("Domain", "Genre - Aggregates")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void InstantiateWithIsActive(bool isActive)
+        {
+            var genreName = "Horror";
+            var genre = new EntityDomain.Genre(genreName, isActive);
+
+            genre.Should().NotBeNull();
+            genre.Name.Should().Be(genreName);
+            genre.IsActive.Should().Be(isActive);
+            genre.CreatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+        }
+
+    }
+}
